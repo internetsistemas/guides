6f567fabb84bca46789c47a9132e8a8e7bc8084b
Gabriel Jordão Trabasso <gabrieljt@gmail.com>
2015-02-12 12:19:33 -0200

    Update sample.rb

--- best-practices/rubyonrails/migrations/sample.rb
+++ best-practices/rubyonrails/migrations/sample.rb
@@ -1,11 +1,15 @@
-# $ rails generate model Product name:string description:text
+# $ rails generate model Product recommended_price:decimal code:string name:string description:text kind:integer
 #
 # 2015012619_create_products.rb
 class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
+      t.decimal :recommended_price, precision: 10, scale: 2
+      t.string :code
       t.string :name
       t.text :description
+      t.integer :kind, index: true
+      
       t.timestamps null: false
     end
   end
