8b4002a1823e53ecb927ea3fc2f043419fbc73bd
Bruno Casali <brunoocasali@gmail.com>
2015-12-15 10:52:37 -0200

    using the new guidelines

--- best-practices/rubyonrails/rspec/sample.rb
+++ best-practices/rubyonrails/rspec/sample.rb
@@ -1,14 +1,14 @@
 # Test With controller spec and rendering views
 #
-RSpec.describe ProductsController, :type => :controller do
+RSpec.describe ProductsController, type: :controller do
   render_views
 
   let!(:valid_session) { {} }
   let!(:product) { create(:product) }
 
-  describe "GET #index" do
+  describe 'GET #index' do
     it "assigns all products as @products" do
-      get :index, {}, valid_session
+      get :index
 
       expect(assigns(:products)).to eq([product])
     end
@@ -17,15 +17,15 @@ end
 
 # Test With request spec and testing body response
 #
-RSpec.describe ProductsController, :type => :request do
+RSpec.describe ProductsController, type: :request do
   render_views
 
   let!(:valid_session) { {} }
   let!(:product) { create(:product) }
 
-  describe "GET #index" do
-    it "assigns all products as @products" do
-      get 'index.json', {}, valid_session
+  describe 'GET #index' do
+    it 'assigns all products as @products' do
+      get 'index.json'
 
       expect(response.body).to eq([product].to_json)
     end
@@ -34,16 +34,16 @@ end
 
 # Example with Capybara + RSpec
 #
-describe "the signin process", :type => :feature do
+describe 'the signin process', type: :feature do
   before :each do
-    User.make(:email => 'user@example.com', :password => 'password')
+    User.make(email: 'user@example.com', password: 'password')
   end
 
-  it "signs me in" do
+  it 'signs me in' do
     visit '/sessions/new'
 
-    fill_in 'Email', :with => 'user@example.com'
-    fill_in 'Password', :with => 'password'
+    fill_in 'Email', with: 'user@example.com'
+    fill_in 'Password', with: 'password'
     click_button 'Sign in'
 
     expect(page).to have_content 'Success'
